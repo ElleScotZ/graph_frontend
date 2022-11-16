2d9d14b8286615cd59b2f1c6e6558242099664b7
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2022-11-16 09:24:46 +0100

    urls

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const url = "http://34.78.47.255:8080/"; // Go app's url. Must include '/' at the end for future use.
+export const url = "34.78.47.255:8080/"; // Go app's url. Must include '/' at the end for future use.
